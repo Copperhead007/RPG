d2a49521b878bce8b9a7ea34b67019960fded94b
Copperhead007 <caelanpriddy@hotmail.com>
2017-10-11 19:40:50 -0400

    Rather Developed

---
+++ Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : Interactable {
+
+	Animator animator;
+
+	bool isOpen;
+	public Item[] items;
+
+	void Start() {
+		animator = GetComponent<Animator> ();
+	}
+
+	public override void Interact ()
+	{
+		base.Interact ();
+		if (!isOpen) {
+			animator.SetTrigger ("Open");
+			StartCoroutine (CollectTreasure ());
+		}
+	}
+
+	IEnumerator CollectTreasure() {
+
+		isOpen = true;
+        int numItems = Random.Range(0, 9);
+		yield return new WaitForSeconds (1f);
+		print ("Chest opened");
+		for(int y = 0; y<=numItems; y++)
+        {
+            Item i = items[Random.Range(0,5)];
+			Inventory.instance.Add (i);
+		}
+	}
+}
